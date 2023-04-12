2cea6fc636b719fd0866b27b24b039ee762e233b
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-04-12 19:01:21 +0300

    fix

--- index.js
+++ index.js
@@ -2,26 +2,22 @@ const todoList = document.getElementById('todoList');
 const newTodoInput = document.getElementById('newTodo');
 const addTodoButton = document.getElementById('addTodo');
 
-// Обработчик события изменения состояния checkbox
 function handleTodoChange(event) {
 	if (event.target.tagName === 'INPUT') {
 		console.log('Checkbox state changed');
 	}
 }
 
-// Обработчик события отправки формы
 function handleFormSubmit(event) {
-	event.preventDefault(); // отменяем действие по умолчанию
-	const newTodoText = newTodoInput.value.trim(); // получаем текст новой задачи и удаляем пробелы
+	event.preventDefault(); 
+	const newTodoText = newTodoInput.value.trim(); 
 	if (newTodoText !== '') {
-		// Создаем новый элемент списка
 		const newTodo = document.createElement('li');
 		newTodo.innerHTML = `<input type="checkbox">${newTodoText}`;
-		todoList.appendChild(newTodo); // добавляем новый элемент в список
-		newTodoInput.value = ''; // очищаем поле ввода
+		todoList.appendChild(newTodo); 
+		newTodoInput.value = ''; 
 	}
 }
 
-// Добавляем обработчики событий
 todoList.addEventListener('change', handleTodoChange);
 addTodoButton.addEventListener('click', handleFormSubmit);
